7d719d556ce739d0710e8019eb78a376d9420878
Remix Workspace <>
2022-08-14 11:25:21 +0100

    Created sfStore and sfGet functions

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -10,4 +10,16 @@ contract StorageFactory {
         SimpleStorage newSimpleStorage = new SimpleStorage();
         simpleStorageArray.push(newSimpleStorage);
     }
+
+    function storageFactoryStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // To interact with a contract within another contract, you need:
+        // Address and Application Binary Interface(ABI) of the contract
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function storageFactoryGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
 }
